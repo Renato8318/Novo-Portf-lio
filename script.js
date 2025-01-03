4fd6845fd5fac2b1efb37a4928e260cfff8056ab
Renato Paiva <paivarenato08@gmail.com>
2025-01-03 15:14:11 -0300

    Projeto final do novo Portfólio Interativo

---
+++ script.js
@@ -0,0 +1,51 @@
+// Criando variáveis para receber os IDs
+let bntMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+// Adicionando um evento às variáveis
+// para abrir menu
+bntMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu');
+});
+
+// para fechar menu
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu');
+});
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu');
+});
+
+// Animações
+const myObserver = new IntersectionObserver((entrada) =>{
+    entrada.forEach( (entry) => {
+        if(entry.isIntersecting === true){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden');
+
+elements.forEach( (element) => myObserver.observe(element));
+
+// Saudações
+// Obter id da saudações
+const greetingElement = document.getElementById("greeting");
+
+// Obter horas do sistema
+const currentHour = new Date().getHours();
+
+// Verificar horas e mostrar mensagem
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia! Sou Renato"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde! Sou Renato"
+    : "Boa noite! Sou Renato";
+
+greetingElement.textContent = greetingMessage;

cee002d17ac3315b0ba52f6a56fd3af171c1254e
Renato Paiva <paivarenato08@gmail.com>
2025-11-20 17:47:42 -0300

    alteração da msg de boas vindas do site

--- script.js
+++ script.js
@@ -45,34 +45,28 @@ const elements = document.querySelectorAll('.hidden');
 
 elements.forEach( (element) => myObserver.observe(element));
 
-// Saudações
-// Obter id da saudações
-const greetingElement = document.getElementById("greeting");
-
-// Obter horas do sistema
-const currentHour = new Date().getHours();
-
-// Verificar horas e mostrar mensagem
-const greetingMessage =
-  currentHour >= 5 && currentHour < 12
-    ? "Olá,Bom dia! Sou Renato Paiva"
-    : currentHour >= 12 && currentHour < 18
-    ? "Olá,Boa tarde! Sou Renato Paiva"
-    : "Olá,Boa noite! Sou Renato Paiva";
-
-greetingElement.textContent = greetingMessage;
-
 // Efeito de Digitação na Faixa de Boas-Vindas
 function typeWriterEffect() {
     const textElement = document.getElementById('typing-text');
     if (!textElement) return;
 
-    const text = "Seja bem-vindo ao meu universo digital!";
-    textElement.innerHTML = text; // Coloca o texto de uma vez
+    // Define a saudação baseada na hora do dia
+    const currentHour = new Date().getHours();
+    let greeting = '';
+
+    if (currentHour >= 5 && currentHour < 12) {
+        greeting = 'Bom dia!';
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greeting = 'Boa tarde!';
+    } else {
+        greeting = 'Boa noite!';
+    }
+
+    textElement.innerHTML = greeting; // Coloca o texto de uma vez
 
     // Calcula a duração da animação baseada no tamanho do texto
-    const typingSpeed = 3.5; // segundos
-    const textLength = text.length;
+    const typingSpeed = 2.5; // segundos
+    const textLength = greeting.length;
 
     // Adiciona a animação de digitação via CSS
     textElement.style.width = `${textLength}ch`; // 'ch' é a largura de um caractere '0'
